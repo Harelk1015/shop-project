847a40cb06b57bc0f6df42f9b32711da68595a45
Harel Kaufman <harelk1015@h-MBP-sl-Harel.lan>
2021-11-15 17:23:01 +0200

    last

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,18 +1,6 @@
 import Login from '../../components/login/Login';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import React from 'react';
 
-function Redirect({ to }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push(to);
-  }, [to]);
-
-  return null;
-}
 
 const LoginPage = () => {
   return <Login></Login>;
